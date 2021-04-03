f81d5f8aa2938183dd454698995badc89a80d3e5
HelloZXB <Hello16369@163.com>
2021-04-03 12:02:47 +0800

    feat: 静态结构

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/stylus/index.styl'
+import fastclick from 'fastclick'
 
 Vue.config.productionTip = false
+
+// 取消移动端点击300ms延迟
+fastclick.attach(document.body)
 
 new Vue({
   router,
