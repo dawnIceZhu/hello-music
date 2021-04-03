df2fec89c3b4a7e4653d27592dbe35529324f843
HelloZXB <Hello16369@163.com>
2021-04-03 20:05:59 +0800

    feat: vue-lazyload懒加载

--- src/main.js
+++ src/main.js
@@ -4,11 +4,17 @@ import router from './router'
 import store from './store'
 import './assets/stylus/index.styl'
 import fastclick from 'fastclick'
+import VueLazyload from "vue-lazyload"
 
 Vue.config.productionTip = false
 
 // 取消移动端点击300ms延迟
 fastclick.attach(document.body)
+
+// or with options
+Vue.use(VueLazyload, {
+  loading: require('@/assets/image/logo.png')
+})
 
 new Vue({
   router,
