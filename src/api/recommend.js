594181f2aee05eb31503943c50472edda9480330
HelloZXB <Hello16369@163.com>
2021-04-03 15:19:43 +0800

    feat: 轮播图数据抓取

---
+++ src/api/recommend.js
@@ -0,0 +1,16 @@
+/**
+ * 封装推荐页api
+ */
+
+import jsonp from '@/assets/js/jsonp'
+import {commonParams, options} from "@/assets/js/config"
+
+export function getRecommend() {
+  const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
+  const data = Object.assign({}, commonParams, {
+    platform: 'h5',
+    uin: 0,
+    needNewCode: 1
+  })
+  return jsonp(url, data, options)
+}
