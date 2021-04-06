8c79193e12fc74ad705474600955aa379febc90f
dawnIceZhu <Hello16369@163.com>
2021-04-06 16:08:38 +0800

    feat: 设置列表top值

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -4,10 +4,10 @@
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
-    <div class="bg-image" :style="bgStyle">
+    <div class="bg-image" :style="bgStyle" ref="bgImage">
       <div class="filter"></div>
     </div>
-    <scroll :data="songs" class="list">
+    <scroll :data="songs" class="list" ref="list">
       <div class="song-list-wrapper">
         <song-list :songs="songs"/>
       </div>
@@ -37,6 +37,9 @@ export default {
       type: String,
       default: ''
     }
+  },
+  mounted() {
+    this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`
   },
   computed: {
     bgStyle() {
