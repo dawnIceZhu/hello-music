10c7c52202cefb19a6669b05bd13e309fdc59bee
dawnIceZhu <Hello16369@163.com>
2021-04-06 13:37:54 +0800

    feat: 歌手数据配置

--- src/views/SingerDetail.vue
+++ src/views/SingerDetail.vue
@@ -5,8 +5,18 @@
 </template>
 
 <script>
+import {mapGetters} from 'vuex'
+
 export default {
-  name: "SingerDetail"
+  name: "SingerDetail",
+  computed: {
+    ...mapGetters([
+      'singer'
+    ])
+  },
+  created() {
+    console.log(this.singer)
+  }
 }
 </script>
 
